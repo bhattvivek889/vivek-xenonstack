34da1441c9d8428c635225e9cc7fd03c6dd9f537
bhattvivek889 <98345084+bhattvivek889@users.noreply.github.com>
2022-09-27 20:36:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 
-mongoose.connect('mongodb+srv://vivek_bhatt:Vivek@2002@cluster0-vatbg.mongodb.net/newdb?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://<DB_USER_NAME>:<DB_PASSWORD>@cluster0-vatbg.mongodb.net/registrationFormHeruko?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, (err) => {
